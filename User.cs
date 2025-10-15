f0bd8152708a44535abfd5ab3a191ff329c1d21c
Pedram <pedram.basim@gmail.com>
2025-10-15 15:24:27 +0200

    Add user to get permisson from Admin

--- User.cs
+++ User.cs
@@ -4,10 +4,12 @@ class User
 {
 	public string SSN; //Social Security Number
 	public string Password;
+	public string Status;
 
-	public User(string ssn, string password)
+	public User(string ssn, string password, string status = "Active")
 	{
 		SSN = ssn;
 		Password = password;
+		Status = status;
 	}
 }
