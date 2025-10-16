829a217d1fc31ec4131c7fd040486af02f1639b6
Arbaz shah <arbazshah@MacBook-Air-6.local>
2025-10-16 14:10:22 +0200

    permission/admin

---
+++ location.cs
@@ -0,0 +1,12 @@
+namespace HealthCare5;
+  class Location
+    {
+        public string Name;
+        public string Description;
+
+        public Location(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
