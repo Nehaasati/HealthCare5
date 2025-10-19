22c7556db7ab5e1e4adbb926cb203f0949228329
Arbaz shah <arbazshah@MacBook-Air-6.local>
2025-10-20 01:00:57 +0200

    accept/deny request

--- User.cs
+++ User.cs
@@ -1,1 +1,23 @@
-namespace App;
+namespace HealthCare5;
+
+public class User
+{
+    public string SSN;
+    public string Password;
+    public Role UserRole; // <-- this is required
+
+    public enum Role
+    {
+        SuperAdmin,
+        Admin,
+        Personnel,
+        Patient
+    }
+
+    public User(string ssn, string password, Role role)
+    {
+        SSN = ssn;
+        Password = password;
+        UserRole = role;
+    }
+}
